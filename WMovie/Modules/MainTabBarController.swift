278b0e31477b093eb0fb3e9bb0146e874755ffd2
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-30 22:07:06 +0300

    code style fixes

--- WMovie/Modules/MainTabBarController.swift
+++ WMovie/Modules/MainTabBarController.swift
@@ -15,19 +15,15 @@ class MainTabBarController: UITabBarController {
         
         let vc1 = UINavigationController(rootViewController: HomeViewController())
         vc1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.circle"), selectedImage: UIImage(systemName: "house.circle.fill"))
-        vc1.title = "Home"
         
         let vc2 = UINavigationController(rootViewController: UpcomingViewController())
         vc2.tabBarItem = UITabBarItem(title: "Coming Soon", image: UIImage(systemName: "play.circle"), selectedImage: UIImage(systemName: "play.circle.fill"))
-        vc1.title = "Coming"
         
         let vc3 = UINavigationController(rootViewController: SearchViewController())
         vc3.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass.circle"), selectedImage: UIImage(systemName: "magnifyingglass.circle.fill"))
-        vc3.title = "Search"
         
         let vc4 = UINavigationController(rootViewController: DownloadsViewController())
         vc4.tabBarItem = UITabBarItem(title: "Downloads", image: UIImage(systemName: "arrow.down.circle"), selectedImage: UIImage(systemName: "arrow.down.circle.fill"))
-        vc4.title = "Downloads"
         
         tabBar.tintColor = .label
         setViewControllers([vc1, vc2, vc3, vc4], animated: true)
