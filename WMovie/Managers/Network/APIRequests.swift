4b16b7ce25da3bc12a29e63552875239a77bae3d
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-30 22:00:31 +0300

    added new requests

--- WMovie/Managers/Network/APIRequests.swift
+++ WMovie/Managers/Network/APIRequests.swift
@@ -33,14 +33,62 @@ struct PopularMoviesRequest: APIRequest {
     var path: String { "/3/movie/popular" }
 }
 
-struct UpcomingMoviesRequest: APIRequest {
+struct PopularTVsRequest: APIRequest {
     typealias Response = [Movie]
     
-    var path: String { "/3/movie/upcoming" }
+    var path: String { "/3/tv/popular"}
 }
 
 struct TopRatedMoviesRequest: APIRequest {
     typealias Response = [Movie]
     
     var path: String { "/3/movie/top_rated" }
+}
+
+struct TopRatedTVsRequest: APIRequest {
+    typealias Response = [Movie]
+    
+    var path: String { "/3/tv/top_rated"}
+}
+
+struct UpcomingMoviesRequest: APIRequest {
+    typealias Response = [Movie]
+    
+    var path: String { "/3/movie/upcoming" }
+    
+    let page: Int
+    
+    var queryItems: [URLQueryItem]? { [URLQueryItem(name: "api_key", value: APIConstants.API_KEY), URLQueryItem(name: "page", value: "\(page)")] }
+    
+    init(page: Int) {
+        self.page = page
+    }
+}
+
+struct SearchMovieRequest: APIRequest {
+    typealias Response = [Movie]
+    //, URLQueryItem(name: "language", value: "en-US")
+    var path: String { "/3/search/movie" }
+    
+    var queryItems: [URLQueryItem]? { [URLQueryItem(name: "api_key", value: APIConstants.API_KEY), URLQueryItem(name: "page", value: "1"), URLQueryItem(name: "include_adult", value: "true"), URLQueryItem(name: "query", value: query)] }
+    
+    let query: String
+    
+    init(query: String) {
+        self.query = query
+    }
+}
+
+struct SearchTVsRequest: APIRequest {
+    typealias Response = [Movie]
+    //, URLQueryItem(name: "language", value: "en-US")
+    var path: String { "/3/search/tv" }
+    
+    var queryItems: [URLQueryItem]? { [URLQueryItem(name: "api_key", value: APIConstants.API_KEY), URLQueryItem(name: "page", value: "1"), URLQueryItem(name: "include_adult", value: "true"), URLQueryItem(name: "query", value: query)] }
+    
+    let query: String
+    
+    init(query: String) {
+        self.query = query
+    }
 }
