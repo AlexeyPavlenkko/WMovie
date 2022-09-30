d6b53fe687d5f016641cdc8dc6508bb0c37034b2
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-30 22:04:05 +0300

    made property "year" to represent Date instead of String

--- WMovie/Model/Movie.swift
+++ WMovie/Model/Movie.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Movie: Decodable, CustomStringConvertible {
     let id: Int
     let title: String
-    let year: String
+    let year: Date
     let rate: Double?
     let mediaType: String?
     let posterImage: String?
@@ -20,7 +20,7 @@ struct Movie: Decodable, CustomStringConvertible {
     
     private enum CodingKeys: String, CodingKey {
         case id
-        case title = "original_title"
+        case title
         case overview
         case year = "release_date"
         case rate = "vote_average"
@@ -38,7 +38,7 @@ struct Movie: Decodable, CustomStringConvertible {
     init() {
         self.id = 0
         self.title = "Default title"
-        self.year = "Default year"
+        self.year = Date()
         self.rate = nil
         self.mediaType = nil
         self.posterImage = nil
@@ -58,8 +58,11 @@ struct Movie: Decodable, CustomStringConvertible {
         self.voteCount = try? container.decodeIfPresent(Int.self, forKey: .voteCount)
         
         let additionalContainer = try decoder.container(keyedBy: AdditionalKeys.self)
-        self.title = (try? container.decodeIfPresent(String.self, forKey: .title)) ?? (try? additionalContainer.decode(String.self, forKey: AdditionalKeys.name)) ?? "Unknown Title"
-        self.year = (try? container.decodeIfPresent(String.self, forKey: .year)) ?? (try? additionalContainer.decode(String.self, forKey: AdditionalKeys.first_air_date)) ?? "Unknown release date"
+        let frmt = DateFormatter()
+        frmt.dateFormat = "dd.MM.yyyy"
+        
+        self.title = (try? container.decodeIfPresent(String.self, forKey: .title)) ?? (try? additionalContainer.decode(String.self, forKey: .name)) ?? "Unknown Title"
+        self.year = (try? container.decodeIfPresent(Date.self, forKey: .year)) ?? (try? additionalContainer.decode(Date.self, forKey: .first_air_date)) ?? frmt.date(from: "01-01-9999")!
     }
     
     var description: String {
