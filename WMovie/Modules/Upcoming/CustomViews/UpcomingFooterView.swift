976586aa239a325de44f335deb5772d096fc0320
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-30 22:11:51 +0300

    added UpcomingFooterView to show that loading more movies is in progress

---
+++ WMovie/Modules/Upcoming/CustomViews/UpcomingFooterView.swift
@@ -0,0 +1,50 @@
+//
+//  UpcomingFooterView.swift
+//  WMovie
+//
+//  Created by Алексей Павленко on 30.09.2022.
+//
+
+import UIKit
+
+class UpcomingFooterView: UITableViewHeaderFooterView {
+    static let identifier = "UpcomingFooterView"
+    
+    private let activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: .large)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.color = .label
+        indicator.hidesWhenStopped = true 
+        return indicator
+    }()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        addSubview(activityIndicator)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        activityIndicator.color = .label
+    }
+    
+    public func showIndicator() {
+        activityIndicator.startAnimating()
+    }
+    
+    public func removeIndicator() {
+        activityIndicator.stopAnimating()
+    }
+}
