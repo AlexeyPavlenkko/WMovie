e84daf854329a372df6796fa43a3329051899a64
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-10-04 17:01:26 +0300

    code style fixes

--- WMovie/Managers/NetworkManager/APIConstants.swift
+++ WMovie/Managers/NetworkManager/APIConstants.swift
@@ -8,13 +8,12 @@
 import Foundation
 
 struct APIConstants {
-    static let tmdbAPI_KEY = "87db726043635956ebb8cde640e28a2f"
+    static let tmdbAPI_KEY = "*********************"
     static let tmdbBaseURL = "https://api.themoviedb.org"
     static let tmdbBaseURLForImage = "https://image.tmdb.org/t/p/w500"
     
-    //main "AIzaSyBJQ7eEEXsdZAbY8n-UBe6qdpxyTnexyW0"
-    //backup AIzaSyCc8CZcH2otf6drKhrSlgBEJWBUsqRg3P4
-    static let ytAPI_KEY = "AIzaSyCc8CZcH2otf6drKhrSlgBEJWBUsqRg3P4"
+
+    static let ytAPI_KEY = "*********************"
     static let ytBaseURL = "https://youtube.googleapis.com/youtube/v3/search"
     static let ytVideoBaseURL = "https://www.youtube.com/embed/"
 }
