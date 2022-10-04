5f350ebe6cf2bf4ecfb6c08e1b455ce5324afd34
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-10-04 16:48:52 +0300

    implemented logic of creation childVCs which injected module builder

--- WMovie/Modules/MainTabBarController.swift
+++ WMovie/Modules/MainTabBarController.swift
@@ -7,28 +7,39 @@
 
 import UIKit
 
-class MainTabBarController: UITabBarController {
-
+final class MainTabBarController: UITabBarController {
+    
+    let moduleBuilder: ModuleBuilderProtocol
+    
+    init(moduleBuilder: ModuleBuilderProtocol) {
+        self.moduleBuilder = moduleBuilder
+        super.init(nibName: nil, bundle: .main)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
         
-        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc1 = moduleBuilder.build(module: .home)
         vc1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.circle"), selectedImage: UIImage(systemName: "house.circle.fill"))
         
-        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc2 = moduleBuilder.build(module: .upcoming)
         vc2.tabBarItem = UITabBarItem(title: "Coming Soon", image: UIImage(systemName: "play.circle"), selectedImage: UIImage(systemName: "play.circle.fill"))
         
-        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc3 = moduleBuilder.build(module: .search)
         vc3.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass.circle"), selectedImage: UIImage(systemName: "magnifyingglass.circle.fill"))
         
-        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        let vc4 = moduleBuilder.build(module: .downloads)
         vc4.tabBarItem = UITabBarItem(title: "Downloads", image: UIImage(systemName: "arrow.down.circle"), selectedImage: UIImage(systemName: "arrow.down.circle.fill"))
         
         tabBar.tintColor = .label
         setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
-
-
+    
+    //MARK: - Deinit
+    deinit { print("DEALLOCATION: \(Self.self)")}
 }
-
